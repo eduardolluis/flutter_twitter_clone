64a5dc48b53fa57e2ab817e8daef009cc42a3bbe
eduardolluis <1132907@est.intec.edu.do>
2026-01-31 11:09:03 -0400

    setting up flutter project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '697e1775003607bb94ec';
+  static const String projectId = '697e00fb003147c212a6';
+  static const String endpoint = 'http://localhost:80/v1';
+}
