57bf1773e4dc80ee3baba9218144707b913a9dc1
eduardolluis <1132907@est.intec.edu.do>
2026-02-01 14:40:49 -0400

    singing up user with appwrite auth

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,39 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/appwrite.dart' as model;
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/core.dart';
+
+abstract class IAuthApi {
+  FutureEither<Account> signUp({
+    required String email,
+    required String password,
+    required String confirmPassword,
+  });
+}
+
+class AuthAPI implements IAuthApi {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.Account> signUp({
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account as model.Account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? "Some unexpected error occurred", stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
+}
