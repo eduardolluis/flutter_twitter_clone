bf9aa7c7ce364bff9e3df499c47f96f6aff0d38b
eduardolluis <1132907@est.intec.edu.do>
2026-02-01 13:49:16 -0400

    signup UI

--- lib/features/auth/view/signup_view.dart
+++ lib/features/auth/view/signup_view.dart
@@ -2,29 +2,31 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/common/rounded_small_button.dart';
 import 'package:twitter_clone/constants/ui_constants.dart';
-import 'package:twitter_clone/features/auth/view/signup_view.dart';
+import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/widget/auth_field.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
-class LoginView extends StatefulWidget {
+class SignUpView extends StatefulWidget {
   static dynamic route() =>
-      MaterialPageRoute(builder: (context) => const LoginView());
-  const LoginView({super.key});
+      MaterialPageRoute(builder: (context) => const SignUpView());
+  const SignUpView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  State<SignUpView> createState() => _SignUpViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _SignUpViewState extends State<SignUpView> {
   final appbar = UiConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     emailController.dispose();
     passwordController.dispose();
+    confirmPasswordController.dispose();
   }
 
   @override
@@ -40,7 +42,12 @@ class _LoginViewState extends State<LoginView> {
                 AuthField(controller: emailController, hintText: "Email"),
                 const SizedBox(height: 25),
                 AuthField(controller: passwordController, hintText: "Password"),
+                const SizedBox(height: 25),
 
+                AuthField(
+                  controller: confirmPasswordController,
+                  hintText: "Confirm Password",
+                ),
                 const SizedBox(height: 45),
                 Align(
                   alignment: Alignment.centerRight,
@@ -54,23 +61,18 @@ class _LoginViewState extends State<LoginView> {
                 const SizedBox(height: 45),
                 RichText(
                   text: TextSpan(
-                    text: "Don't have an account?",
+                    text: "Already have an account?",
                     style: TextStyle(fontSize: 16),
                     children: [
                       TextSpan(
-                        text: " Sign up",
+                        text: " Log in",
                         style: TextStyle(
                           color: Pallete.blueColor,
                           fontSize: 16,
                         ),
                         recognizer: TapGestureRecognizer()
                           ..onTap = () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const SignUpView(),
-                              ),
-                            );
+                            Navigator.push(context, LoginView.route());
                           },
                       ),
                     ],
